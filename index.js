8966aabbbefaf0927300aa7339d805ff12513e3f
Charlie Russo <cparusso@gmail.com>
2018-07-13 17:38:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ const logShout = (aStr) => {
 const logWhisper = (aStr) => {
   console.log(aStr.toLowerCase());
 }
+
+const sayHiToGrandma = (aStr) => {
+  if (aStr === whisper(aStr)) {
+    return "I can't hear you";
+  } else if (aStr === shout(aStr)) {
+    return "YES INDEED!";
+  } else if (aStr === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
